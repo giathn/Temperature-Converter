2e157f65e4d5456dfcab1c0da736529eca710036
android <android>
2023-01-19 12:23:28 -0500

    fix commit

--- app/src/main/java/com/example/temperature/MainActivity.java
+++ app/src/main/java/com/example/temperature/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.temperature;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText inputText;
+    TextView displayText;
+    Button buttonF;
+    Button buttonC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputText = findViewById(R.id.inputText);
+        displayText = findViewById(R.id.displayText);
+        buttonC = findViewById(R.id.buttonC);
+        buttonF = findViewById(R.id.buttonF);
+
+
+
+        buttonF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = inputText.getText().toString();
+                double n = Double.parseDouble(s);
+                displayText.setText(n + " Fahrenheit is " +  (n-32) * (5.0/9) + " Celsius: ");
+            }
+        });
+
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = inputText.getText().toString();
+                double n = Double.parseDouble(s);
+                displayText.setText(n + " Celsius is " +  (n-32) * (5.0/9) + " Fahreinheit: ");
+            }
+        });
+
     }
 }
